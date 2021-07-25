1af7fac979dd53bb01b62e8a3ab3e7539c31943d
sh!zeeg <shizeeque@gmail.com>
2021-07-25 03:55:34 +0300

    add episode_title struct

--- guessit.go
+++ guessit.go
@@ -17,286 +17,292 @@
 package guessit
 
 import (
-  "encoding/json"
-  "os/exec"
+	"encoding/json"
+	"os/exec"
 
-  "github.com/chlorm/go-sys-utils"
+	"github.com/chlorm/go-sys-utils"
 )
 
 // http://guessit.readthedocs.io/en/latest/properties.html
 type Properties struct {
-  // Main properties
-  Type []struct {
-    Value string `json:"value"`
-    Raw string `json:"raw"`
-    Start int `json:"start"`
-    End int `json:"end"`
-  } `json:"type"`
-  Title []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"title,omitempty"`
-  AlternativeTitle []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"alternative_title,omitempty"`
-  Container []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"container,omitempty"`
-  MimeType []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"mimetype,omitempty"`
-  Date []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"date,omitempty"`
-  Year []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"year,omitempty"`
-  ReleaseGroup []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"release_group,omitempty"`
-  Website []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"website,omitempty"`
-  StreamingService []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"streaming_service,omitempty"`
-  // Episode properties
-  Season []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"season,omitempty"`
-  Episode []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"episode,omitempty"`
-  EpisodeCount []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"episode_count,omitempty"`
-  SeasonCount []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"season_count,omitempty"`
-  EpisodeDetails []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"episode_details,omitempty"`
-  EpisodeFormat []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"episode_format,omitempty"`
-  Part []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"part,omitempty"`
-  Version []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"version,omitempty"`
-  //Video properties
-  Format []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"format,omitempty"`
-  ScreenSize []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"screen_size,omitempty"`
-  VideoCodec []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"video_codec,omitempty"`
-  VideoProfile []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"video_profile,omitempty"`
-  VideoApi []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"video_api,omitempty"`
-  // Audio properties
-  AudioChannels []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"audio_channels,omitempty"`
-  AudioCodec []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"audio_codec,omitempty"`
-  AudioProfile []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"audio_profile,omitempty"`
-  // Localization properties
-  Country []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"country,omitempty"`
-  Language []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"language,omitempty"`
-  SubtitleLanguage []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"subtitle_language,omitempty"`
-  // Other properties
-  Bonus []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"bonus,omitempty"`
-  BonusTitle []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"bonus_title,omitempty"`
-  Cd []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"cd,omitempty"`
-  CdCount []struct {
-    Value int `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"cd_count,omitempty"`
-  Crc32 []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"crc32,omitempty"`
-  Uuid []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"uuid,omitempty"`
-  Size []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"size,omitempty"`
-  Edition []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"edition,omitempty"`
-  Film []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"film,omitempty"`
-  FilmTitle []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"film_title,omitempty"`
-  FilmSeries []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"film_series,omitempty"`
-  Other []struct {
-    Value string `json:"value,omitempty"`
-    Raw string `json:"raw,omitempty"`
-    Start int `json:"start,omitempty"`
-    End int `json:"end,omitempty"`
-  } `json:"other,omitempty"`
+	// Main properties
+	Type []struct {
+		Value string `json:"value"`
+		Raw   string `json:"raw"`
+		Start int    `json:"start"`
+		End   int    `json:"end"`
+	} `json:"type"`
+	Title []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"title,omitempty"`
+	EpisodeTitle []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"episode_title,omitempty"`
+	AlternativeTitle []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"alternative_title,omitempty"`
+	Container []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"container,omitempty"`
+	MimeType []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"mimetype,omitempty"`
+	Date []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"date,omitempty"`
+	Year []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"year,omitempty"`
+	ReleaseGroup []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"release_group,omitempty"`
+	Website []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"website,omitempty"`
+	StreamingService []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"streaming_service,omitempty"`
+	// Episode properties
+	Season []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"season,omitempty"`
+	Episode []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"episode,omitempty"`
+	EpisodeCount []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"episode_count,omitempty"`
+	SeasonCount []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"season_count,omitempty"`
+	EpisodeDetails []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"episode_details,omitempty"`
+	EpisodeFormat []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"episode_format,omitempty"`
+	Part []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"part,omitempty"`
+	Version []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"version,omitempty"`
+	//Video properties
+	Format []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"format,omitempty"`
+	ScreenSize []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"screen_size,omitempty"`
+	VideoCodec []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"video_codec,omitempty"`
+	VideoProfile []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"video_profile,omitempty"`
+	VideoApi []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"video_api,omitempty"`
+	// Audio properties
+	AudioChannels []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"audio_channels,omitempty"`
+	AudioCodec []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"audio_codec,omitempty"`
+	AudioProfile []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"audio_profile,omitempty"`
+	// Localization properties
+	Country []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"country,omitempty"`
+	Language []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"language,omitempty"`
+	SubtitleLanguage []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"subtitle_language,omitempty"`
+	// Other properties
+	Bonus []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"bonus,omitempty"`
+	BonusTitle []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"bonus_title,omitempty"`
+	Cd []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"cd,omitempty"`
+	CdCount []struct {
+		Value int    `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"cd_count,omitempty"`
+	Crc32 []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"crc32,omitempty"`
+	Uuid []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"uuid,omitempty"`
+	Size []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"size,omitempty"`
+	Edition []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"edition,omitempty"`
+	Film []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"film,omitempty"`
+	FilmTitle []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"film_title,omitempty"`
+	FilmSeries []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"film_series,omitempty"`
+	Other []struct {
+		Value string `json:"value,omitempty"`
+		Raw   string `json:"raw,omitempty"`
+		Start int    `json:"start,omitempty"`
+		End   int    `json:"end,omitempty"`
+	} `json:"other,omitempty"`
 }
 
 // Returns the output of Guessit (advanced) as a struct
 func Guessit(filename string) (*Properties, error) {
-  var err error
+	var err error
 
-  if _, err = sysutils.SearchEnvPath("PATH", ":", "guessit"); err != nil {
-    return nil, err
-  }
+	if _, err = sysutils.SearchEnvPath("PATH", ":", "guessit"); err != nil {
+		return nil, err
+	}
 
-  guessitOut, err := exec.Command("guessit", "--json", "--advanced", "--enforce-list", filename).Output()
-  if err != nil {
-    return nil, err
-  }
+	guessitOut, err := exec.Command("guessit", "--json", "--advanced", "--enforce-list", filename).Output()
+	if err != nil {
+		return nil, err
+	}
 
-  // Unmarshal JSON into struct
-  var properties *Properties
-  if err = json.Unmarshal(guessitOut, &properties); err != nil {
-    return nil, err
-  }
+	// Unmarshal JSON into struct
+	var properties *Properties
+	if err = json.Unmarshal(guessitOut, &properties); err != nil {
+		return nil, err
+	}
 
-  return properties, nil
+	return properties, nil
 }
